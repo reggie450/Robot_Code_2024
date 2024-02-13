f9aee3c0ff5dc97205de455fea2ab472708eb1b6
ChuckNorrisTheBoris <24ebenway@aos92.org>
2024-02-13 16:25:40 -0500

    Added Swerve stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,8 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
+  public static final CTREConfigs ctreConfigs = new CTREConfigs();
+
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -56,7 +58,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-   // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -92,7 +94,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
-
+  
   /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -22,20 +11,18 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
-import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.LauncherSubsystem;
-//import frc.utils.GamepadUtils;
-import java.util.List;
+import frc.robot.autos.*;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 
 /*
 * This Class contains commands and button maps which call methods in the subsystems
-*/
+ */
 public class RobotContainer {
     // The Weapon subsystems
     private final ArmSubsystem m_arm;
     private final IntakeSubsystem m_intake = new IntakeSubsystem();
-    private final LauncherSubsystem m_launcher = new LauncherSubsystem();
+    private final LauncherSubsystem m_launcher = new LauncherSubsystem();    
 
     // The Driver subsystem
     // private final DriverSubsystem m_robotDrive = new LauncherSubsystem();
@@ -44,6 +31,19 @@ public class RobotContainer {
     XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
     XboxController m_weaponController = new XboxController(OIConstants.kWeaponControllerPort);
 
+    /* Drive Controls */
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Driver Buttons */
+    private final JoystickButton zeroGyro = new JoystickButton(m_driverController, XboxController.Button.kY.value);
+    private final JoystickButton robotCentric = new JoystickButton(m_driverController, XboxController.Button.kLeftBumper.value);
+
+    /* Subsystems */
+    private final Swerve s_Swerve = new Swerve();
+
+
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
         m_arm = new ArmSubsystem(m_weaponController);
@@ -51,31 +51,25 @@ public class RobotContainer {
         configureDriverButtons();
         configureWeaponButtons();
 
-        // Configure default commands
-        /*m_robotDrive.setDefaultCommand(
-            // The left stick controls translation of the robot.
-            // Turning is controlled by the X axis of the right stick.
-            new RunCommand(
-                () ->
-                    m_robotDrive.drive(
-                        -GamepadUtils.squareInput(
-                            m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-                        -GamepadUtils.squareInput(
-                            m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-                        -GamepadUtils.squareInput(
-                            m_driverController.getRightX(), OIConstants.kDriveDeadband),
-                        true,
-                        false),
-                m_robotDrive));
-        */
+
+        s_Swerve.setDefaultCommand(
+            new TeleopSwerve(
+                s_Swerve, 
+                () -> -m_driverController.getRawAxis(translationAxis), 
+                () -> -m_driverController.getRawAxis(strafeAxis), 
+                () -> -m_driverController.getRawAxis(rotationAxis), 
+                () -> robotCentric.getAsBoolean()
+            )
+        );
+
         // set the arm subsystem to run the "runAutomatic" function continuously when no other command is running
         m_arm.setDefaultCommand(new RunCommand(() -> m_arm.runAutomatic(), m_arm));
 
         // set the intake to stop (0 power) when no other command is running
         m_intake.setDefaultCommand(new RunCommand(() -> m_intake.setPower(0.0), m_intake));
-
+    
         // configure the launcher to stop when no other command is running
-        m_launcher.setDefaultCommand(new RunCommand(() -> m_launcher.stopLauncher(), m_launcher));
+        m_launcher.setDefaultCommand(new RunCommand(() -> m_launcher.stopLauncher(), m_launcher));        
     }
 
     /**
@@ -107,48 +101,8 @@ public class RobotContainer {
      *
      * @return the command to run in autonomous
      */
-    /*   public Command getAutonomousCommand() {
-        // Create config for trajectory
-        TrajectoryConfig config =
-            new TrajectoryConfig(
-                    AutoConstants.kMaxSpeedMetersPerSecond,
-                    AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                // Add kinematics to ensure max speed is actually obeyed
-                .setKinematics(DriveConstants.kDriveKinematics);
-
-        // An example trajectory to follow. All units in meters.
-        Trajectory exampleTrajectory =
-            TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, new Rotation2d(0)),
-                config);
-
-        var thetaController =
-            new ProfiledPIDController(
-                AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-        thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-        SwerveControllerCommand swerveControllerCommand =
-            new SwerveControllerCommand(
-                exampleTrajectory,
-                m_robotDrive::getPose, // Functional interface to feed supplier
-                DriveConstants.kDriveKinematics,
-
-                // Position controllers
-                new PIDController(AutoConstants.kPXController, 0, 0),
-                new PIDController(AutoConstants.kPYController, 0, 0),
-                thetaController,
-                m_robotDrive::setModuleStates,
-                m_robotDrive);
-
-        // Reset odometry to the starting pose of the trajectory.
-        m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
-
-        // Run path following command, then stop at the end.
-        return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false, false));
-    } */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return new exampleAuto(s_Swerve);
+    }
 }
