2bc817a609e06ab21feb91c92d51c013850b87f2
ChuckNorrisTheBoris <24ebenway@aos92.org>
2024-02-14 16:52:36 -0500

    Added some intake color sensor code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,11 +3,19 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import javax.sound.sampled.Port;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.SparkRelativeEncoder;
+
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.PIDGains;
@@ -18,7 +26,8 @@ public class IntakeSubsystem extends SubsystemBase {
   private CANSparkMax m_motor;
   private RelativeEncoder m_encoder;
   private SparkPIDController m_controller;
-
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private ColorSensorV3 m_ColorV3 = new ColorSensorV3(i2cPort);
   private boolean m_positionMode;
   private double m_targetPosition;
   private double m_power;
@@ -122,6 +131,50 @@ public class IntakeSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() { // This method will be called once per scheduler run
+/**
+     * The method GetColor() returns a normalized color value from the sensor and can be
+     * useful if outputting the color to an RGB LED or similar. To
+     * read the raw color, use GetRawColor().
+     * 
+     * The color sensor works best when within a few inches from an object in
+     * well lit conditions (the built in LED is a big help here!). The farther
+     * an object is the more light from the surroundings will bleed into the 
+     * measurements and make it difficult to accurately determine its color.
+     */
+    Color detectedColor = m_ColorV3.getColor();
+
+    /**
+     * The sensor returns a raw IR value of the infrared light detected.
+     */
+    double IR = m_ColorV3.getIR();
+
+    /**
+     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
+     * sensor.
+     */
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("IR", IR);
+    SmartDashboard.putBoolean("Stop",
+    detectedColor.red >= .5 && detectedColor.red <= .7 &&
+    detectedColor.green >= .3 && detectedColor.green <= .4 &&
+    detectedColor.blue >= .04 && detectedColor.blue <= .1);
+    /**
+     * In addition to RGB IR values, the color sensor can also return an 
+     * infrared proximity value. The chip contains an IR led which will emit
+     * IR pulses and measure the intensity of the return. When an object is 
+     * close the value of the proximity will be large (max 2047 with default
+     * settings) and will approach zero when the object is far away.
+     * 
+     * Proximity can be used to roughly approximate the distance of an object
+     * or provide a threshold for when an object is close enough to provide
+     * accurate color values.
+     */
+    int proximity = m_ColorV3.getProximity();
+
+    SmartDashboard.putNumber("Proximity", proximity);
+
     // if we've reached the position target, drop out of position mode
     if (m_positionMode && isNearTarget()) {
       m_positionMode = false;
