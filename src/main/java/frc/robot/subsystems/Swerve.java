ea202f19a3c51e586431fc1d9c8cefe17ad44ccf
ChuckNorrisTheBoris <24ebenway@aos92.org>
2024-03-07 17:02:57 -0500

    Updated Slew Values

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -23,8 +23,8 @@ public class Swerve extends SubsystemBase {
     public SwerveDriveOdometry swerveOdometry;
     public SwerveModule[] mSwerveMods;
     public Pigeon2 gyro;
-    public SlewRateLimiter translationLimiter = new SlewRateLimiter(0.6);
-    public SlewRateLimiter StrafeLimiter = new SlewRateLimiter(0.6);
+    public SlewRateLimiter translationLimiter = new SlewRateLimiter(0.9);
+    public SlewRateLimiter StrafeLimiter = new SlewRateLimiter(0.9);
     // public StatsCollection stats = new StatsCollection("SwerveSS");
 
     public Swerve() {
