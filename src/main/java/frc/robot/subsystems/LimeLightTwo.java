69f1410a8c030c74d1b166bca4a465d169d56591
Aaron Collins <aaronwcollins42@gmail.com>
2024-03-14 03:04:07 -0400

    Added method to get distance from limelight

--- src/main/java/frc/robot/subsystems/LimeLightTwo.java
+++ src/main/java/frc/robot/subsystems/LimeLightTwo.java
@@ -5,8 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -49,9 +47,27 @@ public class LimeLightTwo extends SubsystemBase {
      * https://docs.limelightvision.io/en/latest/cs_estimating_distance.html#fixed-angle-camera
      * @return distance to target
      */
-   /*  public double getDistanceFromTarget(){
-        return Constants.LL_SHOT_HEIGHT / Math.abs(Math.toRadians(getYOffset()));
-    } */
+    public double getDistanceFromTarget(){
+        
+        double targetOffsetAngle_Vertical = getYOffset();
+
+        // how many degrees back is your limelight rotated from perfectly vertical?
+        double limelightMountAngleDegrees = 25.0; 
+
+        // distance from the center of the Limelight lens to the floor
+        double limelightLensHeightInches = 20.0; 
+
+        // distance from the target to the floor
+        double goalHeightInches = 60.0; 
+
+        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+        double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+        //calculate distance
+        double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+        //return Constants.LL_SHOT_HEIGHT / Math.abs(Math.toRadians(getYOffset()));
+        return distanceFromLimelightToGoalInches;
+    } 
 
     public double getXOffset(){
         return xOffset.getDouble(0);
@@ -72,6 +88,11 @@ public class LimeLightTwo extends SubsystemBase {
     public void LimeOn(){
         table.getEntry("ledMode").setNumber(3);
     }
+    // https://docs.limelightvision.io/docs/docs-limelight/pipeline-apriltag/apriltags
+    public void FilterTags(){
+
+    }
+
 
     public void CameraMode(){
 
