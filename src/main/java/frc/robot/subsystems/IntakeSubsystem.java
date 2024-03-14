53cbfd4de1b742fb9e3e6aa459642ad01b23a4cd
Aaron Collins <aaronwcollins42@gmail.com>
2024-03-13 23:56:53 -0400

    Removed unnecessary stuff

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -22,82 +22,23 @@ import frc.robot.Constants;
 public class IntakeSubsystem extends SubsystemBase {
 
   private CANSparkMax m_motor;
-  // private RelativeEncoder m_encoder;
-  // private SparkPIDController m_controller;
   
   private DigitalInput inLimit = new DigitalInput(1);
   private int timesRun = 0;
-  // private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  // private ColorSensorV3 m_ColorV3 = new ColorSensorV3(i2cPort);
-  // private boolean m_positionMode;
-  // private double m_targetPosition;
-  // private double m_power;
   public boolean collected = false;
   public boolean running = false;
-  // private boolean firing = false;
   /** Creates a new IntakeSubsystem. */
   public IntakeSubsystem() {
 
     // create a new SPARK MAX and configure it
     m_motor = new CANSparkMax(Constants.Intake.kCanId, MotorType.kBrushless);
     m_motor.setInverted(true);
-   // m_motor.setSmartCurrentLimit(Constants.Intake.kCurrentLimit);
     m_motor.setIdleMode(IdleMode.kCoast);
 
-    /* m_encoder = m_motor.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
-
-    m_controller = m_motor.getPIDController();
-    PIDGains.setSparkMaxGains(m_controller, Constants.Intake.kPositionGains);
-    m_motor.enableSoftLimit(SoftLimitDirection.kForward, false); */
     m_motor.burnFlash();
-
 
   }
 
-   /* public void setPower(double _power) {
-    _power *= .3;
-    m_positionMode = false;
-    m_targetPosition = m_encoder.getPosition();
-    m_power = _power;
-  }  */
-
-    /* public Command feedLauncher(LauncherSubsystem _launcher) {
-    Command newCommand =
-        new Command() {
-          private Timer m_timer;
-
-          @Override
-          public void initialize() {
-            m_timer = new Timer();
-            m_timer.start();
-            setPower(0.0);
-            firing = true;
-          }
-
-          @Override
-          public void execute() {
-            _launcher.runLauncher();
-            if (m_timer.get() > Constants.Intake.kShotFeedTime)
-              setPower(1.0);
-          }
-
-          @Override
-          public boolean isFinished() {
-            return m_timer.get() > Constants.Intake.kShotFeedEnd;
-          }
-
-          @Override
-          public void end(boolean interrupted) {
-            setPower(0.0);
-            collected = false;
-            firing = false;
-          }
-        };
-
-    newCommand.addRequirements(this, _launcher);
-
-    return newCommand;
-  }  */
   public void run(double speed){   
     m_motor.set(speed);
   }
