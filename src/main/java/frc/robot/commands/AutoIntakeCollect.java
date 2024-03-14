b5141c8f5c56a4f1d6c426aece8d087726c31a6f
Aaron Collins <aaronwcollins42@gmail.com>
2024-03-13 23:44:25 -0400

    Updated AutoIntake to work for Auto

---
+++ src/main/java/frc/robot/commands/AutoIntakeCollect.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class AutoIntakeCollect extends Command {
+    IntakeSubsystem s_intake;
+    public AutoIntakeCollect(IntakeSubsystem intake){
+        super();
+        s_intake = intake;
+        addRequirements(s_intake);
+        SmartDashboard.putBoolean("AutoIntakeCollect Running - Debug", true);
+    }
+    @Override
+    public void initialize() {
+        s_intake.running = true;
+    }
+
+    @Override
+    public void execute() {
+        s_intake.run(.3);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return s_intake.getLimitSwitch() || s_intake.collected;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        s_intake.collected = true;
+        s_intake.stop();
+        s_intake.running = false;
+        SmartDashboard.putBoolean("AutoIntakeCollect Running - Debug", false);
+    }
+}
+
