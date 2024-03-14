b5cd1e454c4d6f8ffd5e4267eb47a28512bce1b7
Aaron Collins <aaronwcollins42@gmail.com>
2024-03-14 01:00:10 -0400

    Added Parallel Command for collecting note

---
+++ src/main/java/frc/robot/commands/AutoCollect.java
@@ -0,0 +1,16 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class AutoCollect extends ParallelCommandGroup {
+    IntakeSubsystem s_intake;
+    public AutoCollect(Command traverse, IntakeSubsystem intake){
+        super();
+        s_intake = intake;
+        addRequirements(s_intake);
+        addCommands(new AutoIntakeCollect(intake), traverse);
+    }
+}
+
