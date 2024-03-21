8f37a085b09ffab752b97081754df1ed172af623
ChuckNorrisTheBoris <24ebenway@aos92.org>
2024-03-21 16:26:52 -0400

    Maybe this will work better?

--- src/main/java/frc/robot/commands/ArmGoToTarget.java
+++ src/main/java/frc/robot/commands/ArmGoToTarget.java
@@ -1,6 +1,10 @@
 package frc.robot.commands;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ArmSubsystem;
 
@@ -8,21 +12,17 @@ public class ArmGoToTarget extends Command{
     private ArmSubsystem s_arm;
     private double _setpoint;
     private Timer m_timer;
-    private double elapsedTime = 0.0;
-    private boolean blocked = false;
 
     public ArmGoToTarget(ArmSubsystem arm, double setpoint){
       s_arm = arm;
       _setpoint = setpoint;
+      double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+      _setpoint = rotations;
+      addRequirements(arm);
     }
 
     @Override
     public void initialize() {
-      if (s_arm.running){
-        blocked = true;
-        return;
-      }
-      s_arm.setTargetPosition(_setpoint);
       s_arm.running = true;
       m_timer = new Timer();
       m_timer.start();
@@ -30,23 +30,21 @@ public class ArmGoToTarget extends Command{
 
     @Override
     public void execute() {
-      if (!blocked){
-      elapsedTime = m_timer.get();
-      s_arm.runAutomatic(elapsedTime);
-      }
+      s_arm.SetReference(_setpoint);
     }
 
     @Override
     public boolean isFinished() {
-      return blocked || s_arm.autoIsFinished(elapsedTime);
+      //where we are: s_arm.getEncoderPosition()
+      // where we want to be: _setpoint
+      // How close is close enough: Error
+      return Math.abs(_setpoint - s_arm.getEncoderPosition()) < .001;
     }
 
     @Override
     public void end(boolean interrupted) {
-      if (!blocked){
         s_arm.stop();
         s_arm.running = false;
-      }
     }
 
 }
