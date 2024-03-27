a3f095b5721187d8d5285c13606f6d360eef3573
ChuckNorrisTheBoris <24ebenway@aos92.org>
2024-03-27 14:40:57 -0400

    Added a PID controller to arm subsystem

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -39,6 +39,14 @@ public class ArmSubsystem extends SubsystemBase {
   private SparkLimitSwitch m_reverseLimit;
   public boolean running = false;
 
+
+
+
+  private static final int deviceID = 0;
+  private SparkPIDController m_pidController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
+
   // private StatsCollection stats = new StatsCollection("ArmSS");
   /** Creates a new ArmSubsystem and sets default behaviors */
   public ArmSubsystem() {
@@ -74,6 +82,46 @@ public class ArmSubsystem extends SubsystemBase {
     m_forwardLimit = m_leadmotor.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
     m_reverseLimit = m_leadmotor.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
 
+    /**
+     * In order to use PID functionality for a controller, a SparkPIDController object
+     * is constructed by calling the getPIDController() method on an existing
+     * CANSparkMax object
+     */
+    m_pidController = m_leadmotor.getPIDController();
+  
+    /**
+     * The PID Controller can be configured to use the analog sensor as its feedback
+     * device with the method SetFeedbackDevice() and passing the PID Controller
+     * the CANAnalog object. 
+     */
+    m_pidController.setFeedbackDevice(m_encoder);
+
+    // PID coefficients
+    kP = 1; 
+    kI = 0; //1e-4;
+    kD = 0; //1;
+    kIz = 0; 
+    kFF = 0;
+    kMaxOutput = 1;
+    kMinOutput = -1;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", 0);
   }
 
   /**
@@ -160,11 +208,52 @@ public class ArmSubsystem extends SubsystemBase {
     m_leadmotor.set(-speed);
   }
 
+  public void SetReference (double rotations){
+      m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+  }
+
   @Override
   public void periodic() { // This method will be called once per scheduler run
     //stats.Periodic();
     // SmartDashboard.putBoolean("Forward Limit Switch", m_forwardLimit.isPressed());
     // SmartDashboard.putBoolean("Reverse Limit Switch", m_reverseLimit.isPressed());
     SmartDashboard.putNumber("Arm Encoder", m_encoder.getPosition());
+    // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+     *  com.revrobotics.CANSparkMax.ControlType.kPosition
+     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
+     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+     */
+  
+    SmartDashboard.putNumber("SetPoint", rotations);
+    SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
   }
 }
